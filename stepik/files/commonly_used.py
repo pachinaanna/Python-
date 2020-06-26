cd4b69fc8137eec34772f95292081056bdfb0d2a
Anna <pachinaanna@yandex.ru>
2020-06-26 10:58:57 -0700

    file logic commit

---
+++ stepik/files/commonly_used.py
@@ -0,0 +1,5 @@
+with open('C:/Users/admkn/Desktop/string_2.txt', 'r') as string_2:
+    for line in string_2:
+        line = line.lower().strip()
+            for word in line: 
+                Anna was here
---
+++ stepik/files/string_1.py
@@ -0,0 +1,11 @@
+# def expand_string(s1):
+with open('C:/Users/admkn/Desktop/string_1.txt', 'r') as string_1:
+    s1 = string_1.readline()
+    #     letter = None
+    #     digit = None
+    #     for simbol in s1:
+    #         while simbol.isdigit():
+    #             digit =
+
+# if __name__ == '__main__':
+#     expand_string('a3b4c2b1')
